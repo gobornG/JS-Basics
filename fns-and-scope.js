4722a07bc49759a1e55c3cfc3c31a7e171e078a2
HOME\Garry <gtoborn@hotmail.com>
2017-08-16 22:35:09 -0600

    problem 1 solution

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -5,7 +5,11 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
-
+function isTyler(name) {
+if (name === "Tyler") {
+  return true;
+  }
+}
 //////////////////PROBLEM 2////////////////////
 
 
